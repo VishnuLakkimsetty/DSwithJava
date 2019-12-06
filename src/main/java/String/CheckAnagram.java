fd3972ac2d3036aef742b6679faebd734def622f
Vishnu Lakkimsetty <lakshmi.vishnu.lakkimsetty@oracle.com>
2019-12-06 22:54:19 +0530

    Added code to check Anagram

--- src/main/java/String/CheckAnagram.java
+++ src/main/java/String/CheckAnagram.java
@@ -2,6 +2,34 @@
 
 package main.java.String;
 
-public class CheckAnagram {
+import java.util.Arrays;
 
+public class CheckAnagram {
+public static void main(String[] ars) {
+	String s1 = "SILENT";
+	String s2 = "LISTEN";
+	String s3 = "LISTES";
+	System.out.println(IsAnagram(s1,s2));
+	System.out.println(IsAnagram(s1,s3));
+}
+
+ static boolean IsAnagram(String s1, String s2) {
+	if(s1.length()!=s2.length())
+		return false;
+	else {
+		char[] str1 = s1.toCharArray();
+		char[] str2 = s2.toCharArray();
+		
+		Arrays.sort(str1);
+		Arrays.sort(str2);
+		
+		for(int i=0;i<str1.length;i++) {
+			if(str1[i]==str2[i])
+				continue;
+			else
+				return false;
+		}
+		return true;
+	}
+}
 }
