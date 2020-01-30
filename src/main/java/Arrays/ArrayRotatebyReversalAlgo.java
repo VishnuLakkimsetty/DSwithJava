26bd60a119cc5cf7265db4468b114d2124cff81c
Vishnu Lakkimsetty <lakshmi.vishnu.lakkimsetty@oracle.com>
2020-01-30 20:07:55 +0530

    Added new java files

--- src/main/java/Arrays/ArrayRotatebyReversalAlgo.java
+++ src/main/java/Arrays/ArrayRotatebyReversalAlgo.java
@@ -18,11 +18,11 @@ public class ArrayRotatebyReversalAlgo {
 
 		int[] ar = new int[n];
 
-		for (int i=0;i<ar.length;i++) {
+		for (int i = 0; i < ar.length; i++) {
 			ar[i] = i;
 		}
 
-		for (int i=0;i<ar.length;i++) {
+		for (int i = 0; i < ar.length; i++) {
 			System.out.print(ar[i] + " ");
 		}
 
@@ -31,34 +31,31 @@ public class ArrayRotatebyReversalAlgo {
 		int d = sc.nextInt();
 
 		sc.close();
-		
-		if(d>=ar.length) {
+
+		if (d >= ar.length) {
 			throw new RuntimeException("The number by which the array to be rotated exceeds/equals array size");
 		}
-		
+
 		// Rotating Array by reversal Algorithm
-		reverseArray(ar,0,d-1);
-		reverseArray(ar,d,n-1);
-		reverseArray(ar,0,n-1);
-		
+		reverseArray(ar, 0, d - 1);
+		reverseArray(ar, d, n - 1);
+		reverseArray(ar, 0, n - 1);
+
 		System.out.println("Rotated Array by reversal Algorithm is ::");
-		for (int i=0;i<ar.length;i++) {
+		for (int i = 0; i < ar.length; i++) {
 			System.out.print(ar[i] + " ");
 		}
 	}
 
 	private static void reverseArray(int[] ar, int start, int end) {
-		
+
 		int temp;
-//		for(int i=start;i<end;i++) {
-			while(start<end) {
-				temp = ar[start];
-				ar[start]=ar[end];
-				ar[end]=temp;
-				start++;
-				end--;
-			}
-//		}
-		
+		while (start < end) {
+			temp = ar[start];
+			ar[start] = ar[end];
+			ar[end] = temp;
+			start++;
+			end--;
+		}
 	}
 }
